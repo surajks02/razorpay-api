41ea97e74be9870495cdb6120130b659486ef8b0
surajks02 <surajkkns@gmail.com>
2021-07-14 14:46:31 +0530

    Firstproject

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+$keyId = 'rzp_test_F2pfY1r7fplxW1';
+$keySecret = 'BNa9wZQwMNm4nZIxLp62uNdt';
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+//Database connection datails
+$host ="localhost";
+$username="root";
+$password ="root";
+$dbname ="surajfoundation";
---
+++ payment-form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payment Form</title>
+</head>
+<body style="background-color:aqua;" >
+
+    <form action="pay.php" method="post" >
+        Amount: <input type="text" name="Amount"><br>
+        Name: <input type="text" name="customername"><br>
+        Email: <input type="email" name="email"><br>
+        Contact No: <input type="number" name="contactno"><br>
+        <input type="submit" value="Proceed To Pay">
+    </form>
+</body>
+</html>
